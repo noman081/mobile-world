1e77ea611b945c3223646e81c9898be1b318e850
noman081 <abdullahalnoman642@gmail.com>
2022-02-17 01:12:41 +0600

    After completing calculate portion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-// heey 
+function getInputField(inputFieldId) {
+    const inputFieldText = document.getElementById(inputFieldId + '-field');
+    const inputAmount = parseFloat(inputFieldText.value);
+    return inputAmount;
+}
+
+
+document.getElementById('calculate-button').addEventListener('click', function () {
+    const incomeAmount = getInputField('income');
+    const foodAmount = getInputField('food');
+    const rentAmount = getInputField('rent');
+    const clothAmount = getInputField('cloth');
+
+    const totalExpenses = foodAmount + rentAmount + clothAmount;
+    const newBalance = incomeAmount - totalExpenses;
+
+    document.getElementById('show-expenses').innerText = totalExpenses;
+    document.getElementById('show-balance').innerText = newBalance;
+})
