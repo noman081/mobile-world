0a4aca5a947d228225f3f425f6af703acc79768d
noman081 <abdullahalnoman642@gmail.com>
2022-02-17 19:14:19 +0600

    After checking all

--- js/app.js
+++ js/app.js
@@ -40,18 +40,21 @@ document.getElementById('calculate-button').addEventListener('click', function (
 })
 
 document.getElementById('savings-button').addEventListener('click', function () {
+    document.getElementById('show-savings').innerText = '';
+    document.getElementById('show-remaining').innerText = '';
     const savingsPercentage = getInputField('savings');
     const incomeAmount = getInputField('income');
     const currentBalanceText = document.getElementById('show-balance').innerText;
     const currentBalance = parseFloat(currentBalanceText);
     const savingsAmount = incomeAmount * (savingsPercentage / 100);
     const remainingBalance = currentBalance - savingsAmount;
-    if (savingsAmount > remainingBalance) {
+    if (savingsAmount > currentBalance) {
         alert('You do not have enough balance for savings');
         return 0;
     }
     else {
         document.getElementById('show-savings').innerText = savingsAmount;
     }
+    if (isNaN(currentBalance)) return 0;
     document.getElementById('show-remaining').innerText = remainingBalance;
 })
